2a5d0ff96a1b61ab049f83734c9643da80db4520
Hongying LIU <dadaycm@hotmail.com>
2016-10-12 19:42:15 +0800

    6.2.4 email test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 50, minimum: 6 }
+  validates(:email,
+            presence: true,
+            length: { maximum: 255, minimum: 6 },
+            format: { with: VALID_EMAIL_REGEX })
 end
