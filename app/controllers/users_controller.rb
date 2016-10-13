ac082a3a8fe8eb0ea57a357b154c22dde9c48bd6
Hongying LIU <dadaycm@hotmail.com>
2016-10-13 21:43:48 +0800

    7.3.1-FailedSignup,normally used

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,13 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         # debugger
     end
+
+    def create
+        @user = User.new(params[:user]) # 不是最终的实现方式
+        if @user.save
+        # 处理注册成功的情况
+        else
+          render 'new'
+        end
+    end
 end
