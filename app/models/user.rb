0d8456a2856fa05e7c1f906fd0468c147a7bfa90
Hongying LIU <dadaycm@hotmail.com>
2016-10-13 22:05:05 +0800

    7.3.3SignupFailure message of mistakes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-  # before_save { self.email = email.downcase }
-  before_save { email.downcase! }
-  validates :name, presence: true, length: { maximum: 50, minimum: 6 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email,
-            presence: true,
-            length: { maximum: 255, minimum: 6 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+    # before_save { self.email = email.downcase }
+    before_save { email.downcase! }
+    validates :name, presence: true, length: { maximum: 50, minimum: 5 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email,
+              presence: true,
+              length: { maximum: 255, minimum: 7 },
+              format: { with: VALID_EMAIL_REGEX },
+              uniqueness: { case_sensitive: false }
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 8 }
 end
