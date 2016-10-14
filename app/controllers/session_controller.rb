e61f957b7d9858487f0076e4a0d5b74cdc62b764
Hongying LIU <dadaycm@hotmail.com>
2016-10-14 17:50:50 +0800

    8.1.4 flash invalid combination

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,17 @@
 class SessionController < ApplicationController
-  def new
-  end
+    def new
+    end
+
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+        # 登入用户，然后重定向到用户的资料页面
+        else
+          flash[:danger] = 'Invalid email/password combination'
+          render 'new'
+        end
+    end
+
+    def destroy
+    end
 end
