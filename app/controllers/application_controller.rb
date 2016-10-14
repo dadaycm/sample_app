5a1fb0aaef9249cc782da5fcfe636d878e4f4257
Hongying LIU <dadaycm@hotmail.com>
2016-10-14 19:39:28 +0800

    8.2.1 log_in helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  def hello
-    render html: "hello, regards from LHY, chapter 3"
-
-  end
+    protect_from_forgery with: :exception
+    include SessionHelper
+    def hello
+        render html: 'hello, regards from LHY, chapter 3'
+    end
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,10 +5,11 @@ class SessionController < ApplicationController
     def create
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
-        # 登入用户，然后重定向到用户的资料页面
+            log_in user
+            redirect_to user
         else
-          flash.now[:danger] = 'Invalid email/password combination'
-          render 'new'
+            flash.now[:danger] = 'Invalid email/password combination'
+            render 'new'
         end
     end
 
