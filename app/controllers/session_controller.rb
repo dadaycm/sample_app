72ef7d7ae680cf4f9ef0aab2203972b0f1925b92
Hongying LIU <dadaycm@hotmail.com>
2016-10-14 18:00:59 +0800

    8.1 login failure

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,7 +7,7 @@ class SessionController < ApplicationController
         if user && user.authenticate(params[:session][:password])
         # 登入用户，然后重定向到用户的资料页面
         else
-          flash[:danger] = 'Invalid email/password combination'
+          flash.now[:danger] = 'Invalid email/password combination'
           render 'new'
         end
     end
